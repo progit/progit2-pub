f382f041e4e48baeaa9493a76980e8f54e651341
Jean-Noel Avila <jn.avila@free.fr>
2018-02-18 21:07:12 +0100

    use a bootstrap script instead of requiring future changes to travis
    Unfortunately, the travis.yml file cannot be bootstrapped (yet).

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+rm -f Gemfile.lock Gemfile Rakefile
+wget https://raw.githubusercontent.com/progit/progit2-pub/master/Gemfile
+wget https://raw.githubusercontent.com/progit/progit2-pub/master/Rakefile
