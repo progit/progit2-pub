20cd8013300aa1f8f53c1a6a914702d2285e6d24
Jean-Noel Avila <jn.avila@free.fr>
2018-02-20 21:08:34 +0100

    The gems are not optional
    The gems don't take much processing unless they are explicitely
    used (like in the Rakefile).

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,3 @@
 rm -f Gemfile.lock Gemfile Rakefile
 wget https://raw.githubusercontent.com/progit/progit2-pub/master/Gemfile
 wget https://raw.githubusercontent.com/progit/progit2-pub/master/Rakefile
-
-if [ $TRAVIS_REPO_SLUG = "progit/progit2-zh" ] || [ $TRAVIS_REPO_SLUG = "progit/progit2-ja" ]
-then
-    echo "gem 'asciidoctor-pdf-cjk', '~> 0.1.3'" >> Gemfile
-    echo "gem 'asciidoctor-pdf-cjk-kai_gen_gothic', '~> 0.1.1'" >> Gemfile
-fi
-
